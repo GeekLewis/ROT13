b22377a791f35f00719acb6b7f05d5298d312521
Lewis Miller <lewis.miller@gmail.com>
2021-03-17 22:15:44 -0500

    main part of ROT 13 implimented - Need return string still

--- rot13.py
+++ rot13.py
@@ -1,10 +1,42 @@
 
-def split(word):
-    return [char for char in word] 
+def split(string):
+    print(string)
+    return [char for char in string] 
+
+
+def rot_13(message_in):
+    # split_message = split(message_in)
+    message_out = []
+    for i in message_in:
+        if i.isalpha() == False:
+            message_out.append(i)
+        elif i.isupper() == True:
+            v = ord(i)
+            if v < 78:
+                nv = v+13
+            else:
+                nv = v-13
+            message_out.append(chr(nv))
+        elif i.islower() == True:
+            v = ord(i)
+            if v < 110:
+                nv = v+13
+            else:
+                nv = v-13
+            message_out.append(chr(nv))
+        else:
+            print('Somthing went wrong.')
+
+    print(message_out)
+
+    return('test')
+
 
 def main():
     message = 'Guvf vf n grfg zrffntr, pna lbh qrpbqr vg?'
-    print(split(message))
+    
+    zrffntr = rot_13(message)
+    print(zrffntr)
 
 
 if __name__ == '__main__':
