57caaddae62fa04200d201c0dbeb1fbe90db5456
Lewis Miller <lewis.miller@gmail.com>
2021-03-17 21:01:39 -0500

    initialized file and created split function

---
+++ rot13.py
@@ -0,0 +1,11 @@
+
+def split(word):
+    return [char for char in word] 
+
+def main():
+    message = 'Guvf vf n grfg zrffntr, pna lbh qrpbqr vg?'
+    print(split(message))
+
+
+if __name__ == '__main__':
+    main()
